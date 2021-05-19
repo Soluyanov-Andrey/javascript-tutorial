575dfeba23a98e6696b8d5634de97c01ef1752c2
АндрейСолуянов <soluyanov.a@yandex.ru>
2021-05-19 14:50:56 +0300

    Работа со строками

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+//import "./import_jsFIle/values_types_operators/1" //Значения типы и операторы(Работа с цифрами)
+import "./import_jsFIle/values_types_operators/2" //Значения типы и операторы(Работа с строками)
+
+
+
+
+
+
+
 
 
 
