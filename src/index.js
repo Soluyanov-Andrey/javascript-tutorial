fc7c7f2da0b19d941f32c61cc1abf9ebe5318931
АндрейСолуянов <soluyanov.a@yandex.ru>
2021-05-19 16:31:53 +0300

    Переменные

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-//import "./import_jsFIle/values_types_operators/1" //Значения типы и операторы(Работа с цифрами)
-import "./import_jsFIle/values_types_operators/2" //Значения типы и операторы(Работа с строками)
-
-
-
-
+//import "./import_jsFIle/values_types_operators/1"   //Значения типы и операторы(Работа с цифрами)
+//import "./import_jsFIle/values_types_operators/2"   //Значения типы и операторы(Работа с строками)
+import "./import_jsFIle/values_types_operators/3"   //Переменные
+//import "./import_jsFIle/object/1"                   //Объекты.
 
 
 
