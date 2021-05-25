3272f06892d30296c3d7caf28824aeddf3441e98
АндрейСолуянов <soluyanov.a@yandex.ru>
2021-05-25 20:11:39 +0300

    функии

--- src/import_jsFIle/function/4.js
+++ src/import_jsFIle/function/4.js
@@ -10,11 +10,18 @@ const sum = function(a, b) {
 // Стрелочная функция
 const max = (a, b) => (a > b ? a : b);
 
+// Пример стрелочной функции с одним аргументом.
+const inc_t =(a) => a + 1;
+//Если аргумент 1 то можно записать и так, const inc_t = a => a + 1;
+
+
 // Если стрелочная не в одну строку
 const avg = (a, b) => {
     const s = sum(a, b);
     return s / 2;
 };
+
+
 
 console.log('inc(5) = ' + inc(5));
 console.log('sum(1, 3) = ' + sum(1, 3));
