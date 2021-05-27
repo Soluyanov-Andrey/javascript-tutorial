35783f1e08d3c5832529dc0a58335e5431d238e0
АндрейСолуянов <soluyanov.a@yandex.ru>
2021-05-27 21:02:04 +0300

    refact

--- src/import_jsFIle/draft/1.js
+++ src/import_jsFIle/draft/1.js
@@ -2,19 +2,19 @@
 // setTimeout(function(){console.log("d4")}, 1000);
 
 
-// let promise = new Promise(function(resolve, reject) {
-//     // эта функция выполнится автоматически, при вызове new Promise
-//     function a(b){return b+5};
-//     // через 1 секунду сигнализировать, что задача выполнена с результатом "done"
-//     // setTimeout(() => resolve("done"), 1000);
-//     setTimeout(function(){
-//         resolve(a(10))}, 1000);
-// });
-//
-// promise.then(
-//     function(result) { console.log(result) },
-//     function(error) { /* обработает ошибку */ }
-// );
+let promise = new Promise(function(resolve, reject) {
+    // эта функция выполнится автоматически, при вызове new Promise
+    function a(b){return b+5};
+    // через 1 секунду сигнализировать, что задача выполнена с результатом "done"
+    // setTimeout(() => resolve("done"), 1000);
+    setTimeout(function(){
+        resolve(a(10))}, 1000);
+});
+
+promise.then(
+    function(result) { console.log(result) },
+    function(error) { /* обработает ошибку */ }
+);
 
 
 
