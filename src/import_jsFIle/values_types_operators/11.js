5c1b2d6be23f7d5ef2c6dbdf74b5ccc50e3d3798
АндрейСолуянов <soluyanov.a@yandex.ru>
2021-06-06 22:45:44 +0300

    Типы данных

---
+++ src/import_jsFIle/values_types_operators/11.js
@@ -0,0 +1,21 @@
+/**
+ * Условный оператор if
+ */
+
+if (null){
+    console.log('Yes');
+} else {
+    console.log('Else');
+}
+//----------------------if else
+const a = 25;
+if (a < 16){
+    console.log('no');
+} else if ( a < 24 && a > 16){
+    console.log('yes');
+} else {
+    console.log('error');
+}
+//---------------------Тернарный оператор ?
+const r = a >25 ? 'full': 'empty';
+
