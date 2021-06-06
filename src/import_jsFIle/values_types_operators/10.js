95160d007c68bfb3c4a22633f0ace293d77c1170
АндрейСолуянов <soluyanov.a@yandex.ru>
2021-06-06 21:32:46 +0300

    Типы данных

---
+++ src/import_jsFIle/values_types_operators/10.js
@@ -0,0 +1,19 @@
+/**
+ *
+ * В js существуют следующие типы данныех
+ * Number     15, 3.14, 1e6, NaN
+ * String     'Jon', "Don", `Hi`
+ * Boolean    true, false
+ * Undefined  undefined
+ * Null       null
+ * Symbol     Symbol(), Symbol('Some description')
+ * Object
+ *
+ * Основных типов данных в js 7. Object, Function, Array, ReqEx это не отдельные типы данных
+ * они относятся к типу Object.
+ *
+ * Object     {name: 'Name', age:17}
+ * Function   function run(){...}
+ * Array      [1,2,3,4,5]
+ * ReqEx      /[a-z/
+ */
