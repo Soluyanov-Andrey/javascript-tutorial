40cca88ef6c164467410fe150bda199859b918aa
АндрейСолуянов <soluyanov.a@yandex.ru>
2021-06-06 21:50:30 +0300

    Типы данных

--- src/import_jsFIle/values_types_operators/10.js
+++ src/import_jsFIle/values_types_operators/10.js
@@ -17,3 +17,24 @@
  * Array      [1,2,3,4,5]
  * ReqEx      /[a-z/
  */
+
+/**
+ * Назначить переменную в js можно двумя способами.
+ *                 Literal notation                  With function constructor
+ * Number          const a = 25 ;                    const a = new Number(25);
+ * String          const n = 'Stef';                 const n = new String('Stef');
+ * Boolean         const b = false;                  const n = new Boolean(false);
+ * Symbol          -                                 const su =Symbol('text');
+ * undefined       const u = undefined ;             -
+ * null            const n = null ;                  -
+ * Object          const j = {                       const j = new Object({
+ *                     name: 'Stef',                 ame: 'Stef',
+ *                     age:  17                      age:  17
+ *                 }                                 }
+ * Array           const items =[25,'Jox','false'    const items = new Array(25,'Jox','false');
+ *
+ * Function        function s(){                     const sum = new Function('a', 'b', 'return a + b')
+ *                 return 2;
+ *                 }
+ *
+ */
