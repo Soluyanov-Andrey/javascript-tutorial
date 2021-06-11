7168be2e984b61136cdda73af833f6cc619f8cc1
АндрейСолуянов <soluyanov.a@yandex.ru>
2021-06-11 10:52:22 +0300

    function 11

--- src/import_jsFIle/function/11.js
+++ src/import_jsFIle/function/11.js
@@ -1,111 +1,79 @@
 /**
- * Рекурсия
+ * Рекурсия.Проходим дерево и дабавляем в него новое свойство isAdress
  */
 
-// let state = {
-//     "knot": [{
-//         "ico": "Папка",
-//         "name": "главное меню",
-//         "type": "Папка",
-//         "adress": "0-1",
-//         "presence_сhild": true,
-//
-//         "сhild": [{
-//             "ico": "Папка",
-//             "name": "База знаний",
-//             "type": "Папка",
-//             "adress": "0-1-1",
-//             "presence_сhild":true,
-//
-//             "сhild": [{
-//                 "ico": "Папка",
-//                 "name": "Компоненты",
-//                 "type": "Папка",
-//                 "adress": "0-1-2",
-//                 "presence_сhild":false,
-//
-//                 "сhild": []
-//             },
-//                 {
-//                     "ico": "Папка",
-//                     "name": "Плагины",
-//                     "type": "Папка",
-//                     "adress": "0-1-22",
-//                     "presence_сhild":false,
-//                     "сhild": []
-//                 },
-//                 {
-//                     "ico": "Папка",
-//                     "name": "Модули",
-//                     "type": "Папка",
-//                     "adress": "0-1-23",
-//                     "presence_сhild":false,
-//                     "сhild": []
-//                 }]
-//         },
-//             {
-//                 "ico": "Папка",
-//                 "name": "Отзывы",
-//                 "type": "Папка",
-//                 "adress": "0-1-25",
-//                 "presence_сhild":false,
-//                 "сhild": []
-//             },
-//             {
-//                 "ico": "Папка",
-//                 "name": "Контакты",
-//                 "type": "Папка",
-//                 "adress": "0-1-24",
-//                 "presence_сhild":false,
-//                 "сhild": []
-//             }]
-//     },
-//         {
-//             "ico": "Папка",
-//             "name": "главное меню2",
-//             "type": "Папка",
-//             "adress": "0-2",
-//             "presence_сhild":false,
-//             "сhild": []
-//         }
-//     ]
-//
-// }
+let state = {
+        "ico": "Папка",
+        "name": "главное меню",
+        "type": "Папка",
+        "adress": "0-1",
+        "presence_сhild": true,
 
-let tree ={
-    id: 'id-1',
-    name: 'Name',
-    nodes: [
-        {
-            id: 'id-2',
-            name: 'Name1',
-            nodes: [
+        "сhild": [{
+            "ico": "Папка",
+            "name": "База знаний",
+            "type": "Папка",
+            "adress": "0-1-1",
+            "presence_сhild":true,
+
+            "сhild": [{
+                "ico": "Папка",
+                "name": "Компоненты",
+                "type": "Папка",
+                "adress": "0-1-2",
+                "presence_сhild":false,
+
+                "сhild": []
+            },
                 {
-                    id: 'id-3',
-                    name: 'Name3',
-                    nodes: []
-                }
-            ]
-
-        },{  id: 'id-4',
-            name: 'Name',
-            nodes: [
+                    "ico": "Папка",
+                    "name": "Плагины",
+                    "type": "Папка",
+                    "adress": "0-1-22",
+                    "presence_сhild":false,
+                    "сhild": []
+                },
                 {
+                    "ico": "Папка",
+                    "name": "Модули",
+                    "type": "Папка",
+                    "adress": "0-1-23",
+                    "presence_сhild":false,
+                    "сhild": []
+                }]
+        },
+            {
+                "ico": "Папка",
+                "name": "Отзывы",
+                "type": "Папка",
+                "adress": "0-1-25",
+                "presence_сhild":false,
+                "сhild": []
+            },
+            {
+                "ico": "Папка",
+                "name": "Контакты",
+                "type": "Папка",
+                "adress": "0-1-24",
+                "presence_сhild":false,
+                "сhild": []
+            }],
+    }
 
-                }
-            ]}
-    ],
-}
 
-const adress =["id-2", "id-3"]
+
+
+const adress =["0-1", "0-1-23"]
+
 const bruteForce = (tree, adress) =>{
-    const isAdress = adress.includes(tree.id);
+    const isAdress = adress.includes(tree.adress);
+
     return {
         ...tree,
         isAdress,
-        nodes: tree.nodes.map(childNode => bruteForce(childNode,adress)),
+         сhild: tree.сhild.map(childNode => bruteForce(childNode,adress))
     }
 };
 
-const result = bruteForce(tree, adress);
-
+const result = bruteForce(state, adress);
+console.log(result);
